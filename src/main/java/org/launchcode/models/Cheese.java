99450e0ccb35566339633faf5d488e0907cf2ce1
Michael <32988594+papismurf08@users.noreply.github.com>
2019-01-07 00:53:17 -0500

    Initial Draft

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -6,6 +6,8 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import java.util.List;
+
 /**
  * Created by LaunchCode
  */
@@ -24,7 +26,13 @@ public class Cheese {
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    private CheeseType type;
+    @ManyToOne
+    private Category category;
+
+    @ManyToMany(mappedBy = "cheeses")
+    private List<Menu> menus;
+
+    //private CheeseType type;
 
     public Cheese(String name, String description) {
         this.name = name;
@@ -53,11 +61,11 @@ public class Cheese {
         this.description = description;
     }
 
-    public CheeseType getType() {
-        return type;
-    }
+    //public CheeseType getType() { return type; }
 
-    public void setType(CheeseType type) {
-        this.type = type;
-    }
+    //public void setType(CheeseType type) { this.type = type; }
+
+    public Category getCategory() { return category; }
+
+    public void setCategory(Category category) {this.category = category; }
 }
